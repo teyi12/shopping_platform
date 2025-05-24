da535239e2bc99992f40133810d7e1c5e48677bd
teyi12 <teyi1@yahoo.de>
2025-05-24 20:54:45 +0200

    Initial commit with secrets cleaned

---
+++ shopping_platform/settings.py
@@ -0,0 +1,111 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Charger les variables d'environnement depuis .env
+load_dotenv()
+
+# Base directory du projet
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Clé secrète Django
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# Mode debug (désactive en production)
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
+# Hôtes autorisés
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
+
+# Applications Django
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'accounts',
+    'cart',
+    'orders',
+    'payments',
+    'shop',
+]
+
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# URLconf
+ROOT_URLCONF = 'shopping_platform.urls'
+
+# Templates
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  # si tu utilises un dossier templates
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'shopping_platform.wsgi.application'
+
+# Base de données par défaut (SQLite)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# Validation des mots de passe
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+# Paramètres internationaux
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+# Fichiers statiques
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# https://support.google.com/a/answer/176600?hl=en
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'teyi9lawson9@gmail.com'
+EMAIL_HOST_PASSWORD = 'kl'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+
+# Clés Stripe
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
+STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
+
+# Clé par défaut pour auto-champs
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
