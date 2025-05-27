4b9c1c8c10ee595a3f9830fab475def139d36319
teyi12 <teyi1@yahoo.de>
2025-05-27 12:59:25 +0200

    Ajout du script de build Render

--- shopping_platform/urls.py
+++ shopping_platform/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('danke/', views.danken_view, name='danke'),
     path('accounts/', include('django.contrib.auth.urls')), 
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
