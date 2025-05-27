2cd3d6b2eb634b09d8d2893c5ba2976b54eebb09
teyi12 <teyi1@yahoo.de>
2025-05-27 19:34:47 +0200

    Ajout du script de build Render

--- shopping_platform/settings.py
+++ shopping_platform/settings.py
@@ -72,13 +72,26 @@ TEMPLATES = [
 WSGI_APPLICATION = 'shopping_platform.wsgi.application'
 
 # Base de données (PostgreSQL via Render)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+import dj_database_url
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+
 
 # Validation des mots de passe
 AUTH_PASSWORD_VALIDATORS = [
