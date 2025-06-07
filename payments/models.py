6cbb00aba9c41c7e4d71df146f2ddfb9938ea99a
teyi12 <teyi1@yahoo.de>
2025-06-07 12:46:25 +0200

    Ajout de .gitignore et suppression des fichiers sensibles

--- payments/models.py
+++ payments/models.py
@@ -20,12 +20,14 @@
 
 from django.db import models
 
+from django.utils import timezone  # <- Ajoute ça en haut
+
 class Payment(models.Model):
-    name = models.CharField(max_length=255)  # Name des Karteninhabers
-    email = models.EmailField()  # Kontakt-E-Mail
-    amount = models.DecimalField(max_digits=10, decimal_places=2)  # Betrag
-    stripe_payment_id = models.CharField(max_length=255, blank=True, null=True)  # Stripe Payment ID
-    created_at = models.DateTimeField(auto_now_add=True)  # Erstellungsdatum
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    stripe_payment_id = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)  # <- Modifié
 
     def __str__(self):
         return f"Payment by {self.name} - ${self.amount}"
